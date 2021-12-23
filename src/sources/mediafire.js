036d3152a3c086f555fed82bccf5ec27488247ad
Kristjan Kirpu <kristjan.kirpu@gmail.com>
2021-12-23 21:26:31 +0200

    fix(mediafire): throw on instant download links

--- src/sources/mediafire.js
+++ src/sources/mediafire.js
@@ -8,8 +8,29 @@ const { proxyToAxios } = require('../utils')
 
 exports.domains = ['mediafire.com']
 
+// some mediafire links are instant downloads
+// and that will potentially crash our html parsing or make node run out of memory
+// so this function makes sure that the given link returns "text/html" content-type
+async function checkLinkResponseType (url, proxy) {
+  const { headers } = await axios({
+    url,
+    proxy: proxyToAxios(proxy),
+    method: 'head'
+  })
+
+  // content type is something other than html
+  if (!headers?.['content-type']?.includes('text/html')) {
+    throw new Error(`Expected "text/html" but received "${headers['content-type']}"`)
+  }
+
+  return
+}
+
 exports.get = async (url, proxy) => {
   try {
+    // make sure we are going to be handling html before requesting data
+    await checkLinkResponseType(url, proxy)
+
     const res = await axios({
       url,
       proxy: proxyToAxios(proxy)
